1bab7de2b0db4181d92ddf89a698b9deb4474918
intfloatbool <intfloatbool@gmail.com>
2022-10-25 13:49:39 +0300

    spawning 10t cube

--- src/1_drawCube/src/redCube.cpp
+++ src/1_drawCube/src/redCube.cpp
@@ -28,7 +28,11 @@ GLuint vbo[numVBOs];
 // Allocate variables used in display() function, so they won't need to be allocated during rendering
 
 GLuint mvLoc;
+GLuint vLoc;
 GLuint projLoc;
+GLuint tfLoc;
+GLuint mLoc;
+
 int width;
 int height;
 float aspect;
@@ -59,6 +63,12 @@ void setupVertices(void)
         1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
     };
 
+    // reduce sizes
+    for(int i = 0; i < 128; i++)
+    {
+        vertexPositions[i] *= 0.25f;
+    }
+
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
@@ -85,6 +95,7 @@ void init(GLFWwindow* window)
 
 void display(GLFWwindow* window, double currentTime)
 {
+
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_DEPTH_BUFFER_BIT);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -94,48 +105,52 @@ void display(GLFWwindow* window, double currentTime)
 
     // get the uniform variables for the MV and projection matrices
     mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
+    mLoc = glGetUniformLocation(renderingProgram, "m_matrix");
     // build perspective matrix
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float) width / (float) height;
     float radians = 1.0472f; //60 degrees
     float near = 0.1f;
     float far = 1000.0f;
-    pMat = glm::perspective(radians, aspect, near, far);
 
-    // build view matrix, model matrix and model-view matrix
-    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    for(int i = 0; i < 24; i++)
+    {
+        pMat = glm::perspective(radians, aspect, near, far);
 
-    // use current time to compute different translations in x, y, and z
-    tMat = glm::translate(glm::mat4(1.0f),
-    glm::vec3(sin(0.35f*currentTime)*2.0f, cos(0.52f*currentTime)*2.0f, sin(0.7f*currentTime)*2.0f));
-    rMat = glm::rotate(glm::mat4(1.0f), 1.75f*(float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
-    rMat = glm::rotate(rMat, 1.75f*(float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
-    rMat = glm::rotate(rMat, 1.75f*(float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
-    // the 1.75 adjusts the rotation speed
+        // build view matrix, model matrix and model-view matrix
+        vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+        mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
 
-    mMat = tMat * rMat;
-    
-    mvMat = vMat * mMat;
+        // copy perspective and MV matrices to corresponding uniform variables
+        
+        glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+        glUniformMatrix4fv(mLoc, 1, GL_FALSE, glm::value_ptr(mMat));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-    // copy perspective and MV matrices to corresponding uniform variables
-    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+        tfLoc = glGetUniformLocation(renderingProgram, "tf");
 
-    // associate VBO with the corresponding vertex attribute in the vertex shader
+        float timeFactor = (float) currentTime;
+        glUniform1f(tfLoc, timeFactor);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        // associate VBO with the corresponding vertex attribute in the vertex shader
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 
-    // arg takes from vertShader 'location' : layout (location = 0) in vec3 position;
-    glEnableVertexAttribArray(0);
+        // arg takes from vertShader 'location' : layout (location = 0) in vec3 position;
+        glEnableVertexAttribArray(0);
 
-    // adjust OpenGL settings and draw model
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+        // adjust OpenGL settings and draw model
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+    }
+
+    // to draw once
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);
+
 }
 
 int main(void) { // main() is unchanged from before
